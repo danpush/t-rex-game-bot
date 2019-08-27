ac9ae469d7f79109888a0b30e9da01b922c86fa8
Daniel Apushkinsky <createsdaniel@gmail.com>
2019-08-27 18:20:57 -0400

    Changed jump/duck mechanics
    Previously I had a function for jumping and another ducking, which instead of simulating a key-press just directly called the code that the web-page would run on a jump or duck respectively. This is now gone, and an actual key press simulation will happen.

--- TRexBot.js
+++ TRexBot.js
@@ -1,27 +1,68 @@
-function jump(){
-	Runner.instance_.tRex.setDuck(false);
-	if (!Runner.instance_.tRex.jumping && !Runner.instance_.tRex.ducking) {
-		Runner.instance_.playSound(Runner.instance_.soundFx.BUTTON_PRESS);
-		Runner.instance_.tRex.startJump(Runner.instance_.currentSpeed);
-	}
-}
-
-function duck(){
-	if (Runner.instance_.tRex.jumping) {
-		Runner.instance_.tRex.setSpeedDrop();
-	} else if (!Runner.instance_.tRex.jumping && !Runner.instance_.tRex.ducking) {
-		Runner.instance_.tRex.setDuck(true);
-	}
-}
-
 setInterval(function(){
 	if (Runner.instance_.horizon.obstacles.length > 0){
 		if (Runner.instance_.horizon.obstacles[0].xPos < Runner.instance_.currentSpeed * 25 - Runner.instance_.horizon.obstacles[0].width/2 && Runner.instance_.horizon.obstacles[0].yPos > 75){
-			jump();
+			keyUp(40);
+			keyDown(38);
 		}
 
 		if (Runner.instance_.horizon.obstacles[0].xPos < Runner.instance_.currentSpeed * 30 - Runner.instance_.horizon.obstacles[0].width/2 && Runner.instance_.horizon.obstacles[0].yPos <= 75){
-			duck();
+			keyDown(40);
 		}
 	}
 }, 5);
+
+
+function keyDown(codeKey){
+	// Simulate a key press
+
+	Podium = {};
+
+	var oEvent = document.createEvent('KeyboardEvent');
+
+    Object.defineProperty(oEvent, 'keyCode', {
+    	get : function() {
+    		return this.keyCodeVal;
+    	}
+    });      
+
+    if (oEvent.initKeyboardEvent) {
+    	oEvent.initKeyboardEvent("keydown", true, true, document.defaultView, codeKey, codeKey, "", "", false, "");
+    } else {
+    	oEvent.initKeyEvent("keydown", true, true, document.defaultView, false, false, false, false, codeKey, 0);
+    }
+
+    oEvent.keyCodeVal = codeKey;
+
+    document.body.dispatchEvent(oEvent);
+
+}
+
+
+function keyUp(codeKey) {
+	// Similate a key up
+
+	Podium = {};
+
+	var oEvent = document.createEvent('KeyboardEvent');
+
+    Object.defineProperty(oEvent, 'keyCode', {
+    	get : function() {
+    		return this.keyCodeVal;
+    	}
+    });      
+
+    if (oEvent.initKeyboardEvent) {
+    	oEvent.initKeyboardEvent("keyup", true, true, document.defaultView, codeKey, codeKey, "", "", false, "");
+    } else {
+    	oEvent.initKeyEvent("keyup", true, true, document.defaultView, false, false, false, false, codeKey, 0);
+    }
+
+    oEvent.keyCodeVal = codeKey;
+
+    document.body.dispatchEvent(oEvent);
+}
+
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
